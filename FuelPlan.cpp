f39bb66bec3f86a20f58792fd2d32c34ebf759ea
Carl Wilke <carlwilke@outlook.com>
2020-07-02 15:13:41 +0100

    Class refactoring

--- FuelPlan.cpp
+++ FuelPlan.cpp
@@ -1,42 +1,19 @@
 #include "FuelPlan.h"
 
-class FuelPlan
+FuelPlan::FuelPlan()
 {
-public:
-	int FuelIn;
-	int FuelBurnGround;
-	int FuelRampOut;
-	int FuelUpliftLiter;
-	int FuelUpliftCalculated;
-	int FuelUpliftActual;
-	int FuelUpliftDiff;
-	int FuelDiffLow;
-	int FuelDiffHigh;
-	bool isImperial;
-	bool isVolumeError;
-	bool isPercentError;
-	bool useMassCheck;
-	double FuelGravity;
-	double FuelDifferencePercent;
-	double FuelDiffPercentMargin;
+	isImperial = false;
+	FuelGravity = 0.8;
+	FuelDiffHigh = 2000;
+	FuelDiffLow = 1000;
+	FuelDiffPercentMargin = 0.05;
+	useMassCheck = true;
+}
 
-private:
-
-	FuelPlan()
-	{
-		isImperial = false;
-		FuelGravity = 0.8;
-		FuelDiffHigh = 2000;
-		FuelDiffLow = 1000;
-		FuelDiffPercentMargin = 0.05;
-		useMassCheck = true;
-	}
-
-	FuelPlan(int fuelHigh, int fuelLow, double diffPercent, bool massError)
-	{
-		FuelDiffHigh = fuelHigh;
-		FuelDiffLow = fuelLow;
-		FuelDiffPercentMargin = diffPercent;
-		useMassCheck = massError;
-	}
-};
+FuelPlan::FuelPlan(int fuelHigh, int fuelLow, double diffPercent, bool massError)
+{
+	FuelDiffHigh = fuelHigh;
+	FuelDiffLow = fuelLow;
+	FuelDiffPercentMargin = diffPercent;
+	useMassCheck = massError;
+}
--- FuelPlan.h
+++ FuelPlan.h
@@ -1,2 +1,28 @@
 #pragma once
 
+class FuelPlan
+{
+private:
+	int FuelIn = 0;
+	int FuelBurnGround = 0;
+	int FuelRampOut = 0;
+	int FuelUpliftLiter = 0;
+	int FuelUpliftCalculated = 0;
+	int FuelUpliftActual = 0;
+	int FuelUpliftDiff = 0;
+	int FuelDiffLow = 0;
+	int FuelDiffHigh = 0;
+	bool isImperial = false;
+	bool isVolumeError = false;
+	bool isPercentError = false;
+	bool useMassCheck = true;
+	double FuelGravity = 0.0;
+	double FuelDifferencePercent = 0.0;
+	double FuelDiffPercentMargin = 0.0;
+
+public:
+
+	FuelPlan();
+	FuelPlan(int fuelHigh, int fuelLow, double diffPercent, bool massError);
+
+};
